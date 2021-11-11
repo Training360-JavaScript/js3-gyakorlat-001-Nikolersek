b1669b866c58352c1fbbae5c7b7381bd6b9546e0
Nikol <nikolersek@gmail.com>
2021-11-11 18:07:55 +0100

    Task is done.

--- solutions/app2.js
+++ solutions/app2.js
@@ -1,7 +1,24 @@
+'use strict';
+
+const HU = {
+    date(nowDate){
+        return new Intl.DateTimeFormat('hu-HU').format(nowDate);
+    },
+    curreny(currencyNumber){
+        return new Intl.NumberFormat('hu-HU', {
+            style: 'currency',
+            currency: 'HUF'
+        }).format(currencyNumber);
+    },
+    list(names){
+        let templatedNames = '';
+        names.forEach((name, index) => {
+            templatedNames += name + (index === names.length - 2 ? ' és ' : ', ');
+        });
+        return templatedNames.substring(0, templatedNames.length - 2);
+    }
+};
+    
 
 
-
-
-
-
-// export default HU
+export default HU
